382bd9d92d3cf45f5dd0ed60ae4046cd209a3f3e
Hemant Sopan Patil <hemantpatil.2103@gmail.com>
2026-08-03 23:06:18 +0530

    Time: 1 ms (99.95%), Space: 94.2 MB (92.37%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,6 +1,8 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
+        // int profit = 0;
+
         // for(int i = 0; i < n; i++){
         //     int buy = prices[i];
         //     for(int j = i + 1; j < n; j++){
@@ -15,12 +17,14 @@ class Solution {
         int maxprofit = 0;
         int minprice = prices[0];
 
-        for (int i = 1; i < n; i++) {
+        for(int i = 0; i < n; i++){
             maxprofit = Math.max(maxprofit, prices[i] - minprice);
 
             minprice = Math.min(minprice, prices[i]);
         }
 
         return maxprofit;
+
     }
+
 }
