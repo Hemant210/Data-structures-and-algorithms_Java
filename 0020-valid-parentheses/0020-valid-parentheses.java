2074a1e4118a5c3e5e82563c8bc8d068a065a540
Hemant Sopan Patil <hemantpatil.2103@gmail.com>
2026-08-22 23:17:00 +0530

    Time: 4 ms (36.53%), Space: 43.5 MB (25.72%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,29 +1,29 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();   
+        Stack<Character> st = new Stack<>();
 
-            for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
 
-            // If opening bracket, push to stack
-            if (ch == '(' || ch == '{' || ch == '[') {
+            if (ch == '{' || ch == '[' || ch == '(') {
                 st.push(ch);
             } else {
-                // If stack is empty or not matching
-                if (st.isEmpty()) return false;
+                if (st.isEmpty()) {
+                    return false;
+                }
 
                 char top = st.peek();
 
-                if ((top == '(' && ch == ')') ||
-                    (top == '{' && ch == '}') ||
-                    (top == '[' && ch == ']')) {
-                    st.pop(); // Matching pair
+                if (top == '{' && ch == '}' ||
+                        top == '[' && ch == ']' ||
+                        top == '(' && ch == ')') {
+                    st.pop();
                 } else {
-                    return false; // Not a match
+                    return false;
                 }
             }
         }
 
-        return st.isEmpty(); // All brackets matched
+        return st.isEmpty();
     }
 }
