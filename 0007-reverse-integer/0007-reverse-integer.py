63b9777a3eff9daf6ff65842a791a43233c6d844
Hemant Sopan Patil <hemantpatil.2103@gmail.com>
2026-09-09 23:22:27 +0530

    Time: 17 ms (59.22%), Space: 12.5 MB (14.03%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverse(self, x):
+        sign = -1 if x < 0 else 1
+        x_abs = abs(x)
+        reversed_x = int(str(x_abs)[::-1])
+        result = sign * reversed_x
+
+        # Check for 32-bit signed integer overflow
+        if result < -2**31 or result > 2**31 - 1:
+            return 0
+        return result
